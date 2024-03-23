c641b3eda9987a46ef5931856592d2b9b011f5bd
pHnkvr <rezuqqqqq@gmail.com>
2024-03-23 04:00:28 +0200

    lesson2 commit

---
+++ src/homework2/task4.java
@@ -0,0 +1,14 @@
+package homework2;
+
+public class task4 {
+    public static void main(String[] args) {
+       int a = 7;
+       int b = 9;
+       a = a + b; //16
+       b = a - b; // 16-9  = 7
+       a = a - b; // 16-7 = 9
+        System.out.println("a = "+ a);
+        System.out.println("b = " +b);
+
+    }
+}
