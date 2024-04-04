596b128c8ed825354a2a870a964881d3d7467bb9
pHnkvr <rezuqqqqq@gmail.com>
2024-04-04 18:23:33 +0300

    lesson4 commit

--- src/homework5/Homework5Task4.java
+++ src/homework5/Homework5Task4.java
@@ -8,7 +8,7 @@ public class Homework5Task4 {
         String [] names = {"Петя", "Маша", "Алёна", "Федя", "Саша", "Антон", "Глеб"};
         int [] time = {10, 12, 14, 16, 18, 20};
         String [] building = {"школу", "магазин", "церковь", "тренажерный зал", "кино", "поликлинику"};
-        String result = "{NAME} будет идти в {BUILDING} в {TIME}";
+        String result = "{NAME} будет идти в {BUILDING} в {TIME}:00";
         Scanner indexOfName = new Scanner(System.in);
         Scanner indexOfTime = new Scanner(System.in);
         Scanner indexOfBuildings = new Scanner(System.in);
@@ -19,14 +19,10 @@ public class Homework5Task4 {
         }
 
         System.out.println("Введите три числа через ENTER");
+
         int N = indexOfName.nextInt();
         int T = indexOfTime.nextInt();
         int B = indexOfBuildings.nextInt();
-
-
-
-
-
 
 
         System.out.println(result.replace("{NAME}", names[N]).replace("{BUILDING}", building[B]).replace("{TIME}", str[T]));
