4d65d9457cf5b3a4906182241453da064f32b0cc
pHnkvr <rezuqqqqq@gmail.com>
2024-04-09 17:20:32 +0300

    lesson7 commit

--- src/homework5/Homework5Task4.java
+++ src/homework5/Homework5Task4.java
@@ -1,6 +1,6 @@
 package homework5;
 
-import java.util.Arrays;
+
 import java.util.Scanner;
 
 public class Homework5Task4 {
@@ -8,26 +8,30 @@ public class Homework5Task4 {
         String [] names = {"Петя", "Маша", "Алёна", "Федя", "Саша", "Антон", "Глеб"};
         int [] time = {10, 12, 14, 16, 18, 20};
         String [] building = {"школу", "магазин", "церковь", "тренажерный зал", "кино", "поликлинику"};
-        String result = "{NAME} будет идти в {BUILDING} в {TIME}";
-        Scanner indexOfName = new Scanner(System.in);
-        Scanner indexOfTime = new Scanner(System.in);
-        Scanner indexOfBuildings = new Scanner(System.in);
-
+        String result = "{NAME} будет идти в {BUILDING} в {TIME}:00";
         String[] str = new String[time.length];
         for(int i = 0; i<time.length; i++){
             str[i]= String.valueOf(time[i]);
         }
 
         System.out.println("Введите три числа через ENTER");
-        int N = indexOfName.nextInt();
-        int T = indexOfTime.nextInt();
-        int B = indexOfBuildings.nextInt();
-
-
-
-
-
-
+        int indexOfName = new Scanner(System.in).nextInt();
+        int indexOfTime = new Scanner(System.in).nextInt();
+        int indexOfBuildings = new Scanner(System.in).nextInt();
+        if (indexOfName<0||indexOfName>6){
+            System.out.println("Введите валидное значение имени от 0 до 6");
+            indexOfName = new Scanner(System.in).nextInt();
+        }
+        if (indexOfTime<0||indexOfTime>5){
+            System.out.println("Введите валидное значение времени от 0 до 6");
+            indexOfTime = new Scanner(System.in).nextInt();
+        } if (indexOfBuildings<0||indexOfBuildings>5){
+            System.out.println("Введите валидное значение здания от 0 до 6");
+            indexOfBuildings = new Scanner(System.in).nextInt();
+        }
+        int N = indexOfName;
+        int T = indexOfTime;
+        int B = indexOfBuildings;
 
         System.out.println(result.replace("{NAME}", names[N]).replace("{BUILDING}", building[B]).replace("{TIME}", str[T]));
 
