c0e46a6ca001eb0afa9b1cc1b4bd641bfb9c2ea5
pHnkvr <rezuqqqqq@gmail.com>
2024-04-11 17:02:48 +0300

    lesson8 commit

---
+++ src/homework8/ConspectMain.java
@@ -0,0 +1,10 @@
+package homework8;
+
+public class ConspectMain {
+    public static void main(String[] args) {
+    Conspect conspect = new Conspect("Иван", "Иванов", "Иванович", "ООП", 100, 1978, "Черный", "MiT" );
+    String consp = conspect.toString();
+        System.out.println(consp);
+    }
+
+}
