3af5a773b2251c740635cc74cdf05de384a7b1c4
pHnkvr <rezuqqqqq@gmail.com>
2024-04-16 17:27:14 +0300

    lesson10 commit

--- src/homework9/Calculator.java
+++ src/homework9/Calculator.java
@@ -3,54 +3,11 @@ package homework9;
 import java.util.Scanner;
 
 public class Calculator {
-    public void start() {
-        System.out.println("Калькулятор запущено");
-        Scanner scanner = new Scanner(System.in);
-
-        while (true) {
-            System.out.print("Введіть дію (+, -, *, /) або 'Stop' для завершення: ");
-            String doMath = scanner.nextLine();
-
-            if (doMath.equalsIgnoreCase("stop")) {
-                System.out.println("Калькулятор закрито");
-                break;
-            }
-
-            if (!bool(doMath)) {
-                System.out.println("Неправильна дія. Будь ласка, введіть одну з опцій: +, -, *, /");
-                continue;
-            }
-
-            System.out.print("Введіть два числа через пробіл: ");
-            double num1, num2;
-            num1 = scanner.nextDouble();
-            num2 = scanner.nextDouble();
-            scanner.nextLine(); // Очищення буфера
-
-            switch (doMath) {
-                case "+":
-                    summ(num1, num2);
-                    break;
-                case "-":
-                    minus(num1, num2);
-                    break;
-                case "*":
-                    multiply(num1, num2);
-                    break;
-                case "/":
-                    division(num1, num2);
-                    break;
-            }
-        }
-
-        scanner.close();
-    }
-
     private boolean bool(String action) {
         return "+-*/".contains(action);
     }
 
-    private void summ(double num1, double num2) {
+    private void sum(double num1, double num2) {
         double result = num1 + num2;
         System.out.println("Результат: " + result);
     }
@@ -67,10 +24,52 @@ public class Calculator {
 
     private void division(double num1, double num2) {
         if (num2 == 0) {
-            System.out.println("Помилка: ділення на нуль!");
+            System.out.println("Деление на ноль запрещено");
         } else {
             double result = num1 / num2;
             System.out.println("Результат: " + result);
         }
+    }
+    public void start() {
+        System.out.println("Калькулятор запущен");
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Введите символ (+, -, *, /) или 'Stop' для завершенния работы калькулятора: ");
+            String doMath = scanner.nextLine();
+
+            if (doMath.equalsIgnoreCase("stop")) {
+                System.out.println("Калькулятор закрыт");
+                break;
+            }
+
+            if (!bool(doMath)) {
+                System.out.println("Неверный символ, введите один из перечисленных (+, -, *, /)");
+                continue;
+            }
+
+            System.out.print("Введите два числа через пробел: ");
+            double num1, num2;
+            num1 = scanner.nextDouble();
+            num2 = scanner.nextDouble();
+            scanner.nextLine(); //
+
+            switch (doMath) {
+                case "+":
+                    sum(num1, num2);
+                    break;
+                case "-":
+                    minus(num1, num2);
+                    break;
+                case "*":
+                    multiply(num1, num2);
+                    break;
+                case "/":
+                    division(num1, num2);
+                    break;
+            }
+        }
+
+        scanner.close();
     }
 }
