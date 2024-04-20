43cc6992cb6f4a391d6d6e82bc084b59ad927108
pHnkvr <rezuqqqqq@gmail.com>
2024-04-21 00:01:31 +0300

    lesson10 commit

--- src/homework10/Human.java
+++ src/homework10/Human.java
@@ -1,4 +1,143 @@
 package homework10;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Human {
+    private String name;
+    private String surname;
+    private int year;
+    private int iq;
+    private Pet pet;
+    private Human mother;
+    private Human father;
+    private Family family;
+    public void greetPet(){
+        System.out.println("Hello, "+pet.getName()+ "!");
+    }
+    public void describePet(){
+        String trickDescription = pet.getTrickLevel() > 50 ? "дуже хитрий" : "майже не хитрий";
+        System.out.println("У мене є " + pet.getSpecies() + ", їй " + pet.getAge() + " років, він " + trickDescription + ".");
+    }
+
+    public Human(String name, String surname, int year) {
+        this.name = name;
+        this.surname = surname;
+        this.year = year;
+    }
+
+    public Human(String name, int year, Human mother, Human father) {
+        this.name = name;
+        this.year = year;
+        this.mother = mother;
+        this.father = father;
+    }
+
+    public Human(String name, String surname, int year, int iq, Pet pet, Human mother, Human father, Family family) {
+        this.name = name;
+        this.surname = surname;
+        this.year = year;
+        this.iq = iq;
+        this.pet = pet;
+        this.mother = mother;
+        this.father = father;
+        this.family = family;
+    }
+
+    public Human() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getIq() {
+        return iq;
+    }
+
+    public void setIq(int iq) {
+        this.iq = iq;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+
+    public Human getMother() {
+        return mother;
+    }
+
+    public void setMother(Human mother) {
+        this.mother = mother;
+    }
+
+    public Human getFather() {
+        return father;
+    }
+
+    public void setFather(Human father) {
+        this.father = father;
+    }
+
+    public Family getFamily() {
+        return family;
+    }
+
+    public void setFamily(Family family) {
+        this.family = family;
+    }
+
+    @Override
+    public String toString() {
+        String motherName = mother != null ? mother.getName() + " " + mother.getSurname() : "Unknown";
+        String fatherName = father != null ? father.getName() + " " + father.getSurname() : "Unknown";
+        return "Human{name='" + name + "', surname='" + surname + "', year=" + year + ", iq=" + iq + ", mother=" + motherName + ", father=" + fatherName + ", pet=" + pet + "}";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Human human = (Human) o;
+        if (year != human.year) return false;
+        if (iq != human.iq) return false;
+        if (!name.equals(human.name)) return false;
+        if (!surname.equals(human.surname)) return false;
+        return Arrays.equals(pet.getHabits(), human.pet.getHabits());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + surname.hashCode();
+        result = 31 * result + year;
+        result = 31 * result + iq;
+        result = 31 * result + Arrays.hashCode(pet.getHabits());
+        return result;
+    }
 }
+
