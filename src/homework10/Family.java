0c5bf8ddc1737239dd00ddc35b9ae8daa3f9ba64
pHnkvr <rezuqqqqq@gmail.com>
2024-04-21 00:11:50 +0300

    lesson10 commit

--- src/homework10/Family.java
+++ src/homework10/Family.java
@@ -1,4 +1,50 @@
 package homework10;
 
 public class Family {
+    private Human mother;
+    private Human father;
+    private Human [] children;
+    private Pet pet;
+    private int childrenCount;
+
+    public Family(Human mother, Human father) {
+        this.mother = mother;
+        this.father = father;
+        this.children = new Human[5];
+        this.childrenCount=0;
+    }
+
+    public Human getFather() {
+        return father;
+    }
+
+    public void setFather(Human father) {
+        this.father = father;
+    }
+
+    public Human[] getChildren() {
+        return children;
+    }
+
+    public void setChildren(Human[] children) {
+        this.children = children;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+
+    public int getChildrenCount() {
+        return childrenCount;
+    }
+
+    public void setChildrenCount(int childrenCount) {
+        this.childrenCount = childrenCount;
+    }
+
+
 }
