246d3e03a59b99354fb1e3adbc99c48b16938d5a
pHnkvr <rezuqqqqq@gmail.com>
2024-04-24 18:23:13 +0300

    lesson10 commit

--- src/homework8/Homework8Task1.java
+++ src/homework8/Homework8Task1.java
@@ -2,24 +2,23 @@ package homework8;
 
 
 public class Homework8Task1 {
-  private int[] numbers;
-  public Homework8Task1(int[]numbers){
-      this.numbers = numbers;
-  }
-  public void printEven(){
-      System.out.println("Четные числа:");
-      for (int number: numbers){
-          if (number % 2 ==0){
-              System.out.print(number+ " ");
-          }
-      }
-  }
-  public void printOdd(){
-      System.out.println("\nНечетные числа:");
-      for (int number: numbers){
-          if (number % 2 !=0){
-              System.out.print(number+ " ");
-          }
-      }
-  }
+
+    public void printEvenAndOdd(int[] numbers) {
+
+        System.out.println("Четные числа:");
+        for (int number : numbers) {
+            if (number % 2 == 0) {
+                System.out.print(number + " ");
+            }
+        }
+
+
+        System.out.println("\nНечетные числа:");
+        for (int number : numbers) {
+            if (number % 2 != 0) {
+                System.out.print(number + " ");
+            }
+        }
+    }
 }
+
--- src/homework8/Homework8Task1Main.java
+++ src/homework8/Homework8Task1Main.java
@@ -3,8 +3,7 @@ package homework8;
 public class Homework8Task1Main {
     public static void main(String[] args) {
         int [] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        Homework8Task1 printNumbers = new Homework8Task1(numbers);
-        printNumbers.printEven();
-        printNumbers.printOdd();
+        Homework8Task1 printNumbers = new Homework8Task1();
+        printNumbers.printEvenAndOdd(numbers);
     }
 }
